503eaf889303a0ab6945e21fde8a898b1d94b761
Waleeed-sheikh <waleedshaikha779@gmail.com>
2024-10-03 03:58:48 +0500

    Task 70% done !

---
+++ interntest/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+import reportWebVitals from './reportWebVitals';
+import PersonalInformationform from './personalForm';
+
+import "./style.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PersonalInformationform/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
